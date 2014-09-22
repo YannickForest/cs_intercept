814717eede14015deee060e98b5e92a2341bf4b5
Ian Onymous <yannick@variousthingslive.com>
2014-09-21 21:29:35 -0500

    adjusted to enable permanent staging instance access control

--- intercept.php
+++ intercept.php
@@ -2,15 +2,20 @@
 // set up your variables here
 define('ENV_MODE', 'dev'); // set to 'dev' to enable access control
 define('PASSWORD', 'password'); // set to the actual password you want to use 
-define('LIVE', 'live.careersite.com'); // set to the final sub/domain used by the live instance
-define('STAGING', 'staging.careersite.com'); // set to the domain used by the staging instance
+define('LIVE', 'careers.company.com'); // set to the final sub/domain used by the live instance
+define('STAGING', 'staging.company.com'); // set to the domain used by the staging instance
 
 // branching
-$current = $_SERVER['HTTP_HOST'];
+$auth = $_SESSION['authorized'] || false;
+$mode = ENV_MODE;
 session_start();
-if (	($current == LIVE || $current == STAGING)
-			&& ENV_MODE === 'dev'
-			&& !$_SESSION['authorized']
-		) {
-	require 'intercept/index.php';
+$current = $_SERVER['HTTP_HOST'];
+switch($current) {
+	case LIVE :
+		if ($mode === 'dev') require 'intercept/index.php';
+	case STAGING :
+		if (!$auth) require 'intercept/index.php';
+		break;
+	default:
+		break;
 }
--- intercept/index.php
+++ intercept/index.php
@@ -1,18 +1,20 @@
 <?php
-switch ($current) {
-	case LIVE:
-		header("Location: //". STAGING ."/");
-		break;
-	case STAGING:
-		if (isset($_POST['password']) && $_POST['password'] === PASSWORD) {
-			$_SESSION['authorized'] = true;
+if (!$auth) {
+	switch ($current) {
+		case LIVE:
+			header("Location: //". STAGING ."/");
+			break;
+		case STAGING:
+			if (isset($_POST['password']) && $_POST['password'] === PASSWORD) {
+				$_SESSION['authorized'] = true;
+				return;
+			} else {
+				$_SESSION['authorized'] = false;
+				require 'index.html';
+				exit();
+			}
+			break;
+		default:
 			return;
-		} else {
-			$_SESSION['authorized'] = false;
-			require 'index.html';
-			exit();
-		}
-		break;
-	default:
-		return;
+	}
 }
